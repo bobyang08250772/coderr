17f484ac96e45aae66349a6b43c3892143b8bdf1
bobyang08250772 <87290044+bobyang08250772@users.noreply.github.com>
2025-08-10 16:39:17 +0200

    config changed

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+FORCE_SCRIPT_NAME = '/coderr'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
